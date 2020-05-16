b9e9f042921641b86985833d4b7c750b6d1c83b5
Frank B. Brokken <f.b.brokken@rug.nl>
2020-05-16 22:29:18 +0200

    5.05.00 run-time tests successfully completed

--- bobcat/config/find1.f
+++ bobcat/config/find1.f
@@ -1,10 +1,10 @@
-inline Config::const_iterator Config::find(string const &needle, 
+inline Config::const_iterator Config::find(std::string const &needle, 
                                             const_iterator const &from) const
 {
     return d_ptr->find(needle, from);
 }
 
-inline Config::const_iterator Config::find(string const &needle) const
+inline Config::const_iterator Config::find(std::string const &needle) const
 {
     return find(needle, begin());
 }
