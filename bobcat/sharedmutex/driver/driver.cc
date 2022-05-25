fd219d27046c16ff67586b929d686e64d3afc202
Frank B. Brokken <f.b.brokken@rug.nl>
2022-05-25 10:15:04 +0200

    wip on the shared* man-pages

---
+++ bobcat/sharedmutex/driver/driver.cc
@@ -0,0 +1,83 @@
+#include <sys/types.h>
+#include <signal.h>
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <bobcat/fork>
+#include <bobcat/semaphore>
+#include <bobcat/sharedsegment>
+
+using namespace std;
+using namespace FBB;
+
+class Wait: public Fork
+{
+    SharedSegment *d_shared;
+    int d_id;
+    SharedMutex *d_mutex;
+
+    public:
+        Wait();
+        ~Wait();
+        void childProcess() override;
+        void parentProcess() override;
+};
+
+Wait::Wait()
+:
+    d_shared(SharedSegment::create(&d_id, 1, 100, 0700)),
+    d_mutex(new (d_shared) FBB::SharedMutex)
+{
+    cout << "shared memory ID = " << d_id << '\n';
+}
+
+Wait::~Wait()
+{
+    d_mutex->~SharedMutex();
+    SharedSegment::detach(d_shared);
+    cout << "detached the shared memory\n";
+}
+
+void Wait::childProcess()
+{
+    Semaphore waiter{0};
+
+    while (true)
+    {
+        waiter.wait_for(chrono::seconds(2));
+        d_mutex->lock();
+        cout << "child hello\n";
+        d_mutex->unlock();
+    }
+}
+
+void Wait::parentProcess()
+{
+    string line;
+    do
+    {
+        cout << "press enter to allow the parent to locck\n";
+        cin.ignore(100, '\n');
+        d_mutex->lock();
+        cout << "parent has the lock, press enter to continue "
+                "(to end: some input)\n";
+        getline(cin, line);
+        d_mutex->unlock();
+    }
+    while (line.empty());
+
+    kill(pid(), SIGTERM);
+}
+
+        
+int main()
+{
+    Wait waiter;
+    waiter.fork();
+}
+
+
+
+
+
