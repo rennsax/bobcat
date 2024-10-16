03553e77194f9089080d9a035ea36afc9f2403dd
Bojun Ren <bj.ren.coding@outlook.com>
2024-10-16 14:07:31 +0800

    Add error handling compatibility for strerror_r
    Improves the error handling logic by making it compatible with different
    versions of `strerror_r`. It uses conditional compilation to check if the
    behavior adheres to XSI-compliant conventions (_POSIX_C_SOURCE or _XOPEN_SOURCE)
    and employs the appropriate version of `strerror_r`. This change ensures proper
    error message retrieval and enhances portability across different system
    standards.

--- bobcat/exception/errnodescr.cc
+++ bobcat/exception/errnodescr.cc
@@ -9,8 +9,13 @@ std::ostream &errnodescr(std::ostream &out)
     {
         char *buffer = new char[Exception::STRERROR_BUFSIZE];
 
+#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
         if (char *cp = strerror_r(errno, buffer, Exception::STRERROR_BUFSIZE))
             out << cp;
+#else
+        if (strerror_r(errno, buffer, Exception::STRERROR_BUFSIZE) == 0)
+            out << buffer;
+#endif
         else
         {
             out << "internal error: strerror_r failed with errno = " << errno;
