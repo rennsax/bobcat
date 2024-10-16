ce49ff7f51214aed95c821aada036851c3ee8545
Bojun Ren <bj.ren.coding@outlook.com>
2024-10-16 15:54:29 +0800

    Fix definition for SIGRTMAX
    Darwin doesn't define `SIGRTMAX`.

--- bobcat/signal/verify.cc
+++ bobcat/signal/verify.cc
@@ -4,6 +4,10 @@
 // fails to implement the POSIX standard:
 
 #ifndef SIGRTMAX
+// Darwin doesn't define SIGRTMAX.
+#ifndef _NSIG
+#define _NSIG 32
+#endif
     #define SIGRTMAX _NSIG
 #endif        
 
