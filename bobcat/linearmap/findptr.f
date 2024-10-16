f2136a64363189834348f15449a50878ca6838ce
Bojun Ren <bj.ren.coding@outlook.com>
2024-10-16 16:52:09 +0800

    Fix findPtr in LinearMap

--- bobcat/linearmap/findptr.f
+++ bobcat/linearmap/findptr.f
@@ -1,12 +1,10 @@
 // value_type *findPtr(Key const &key) const
 
 template <typename Key, typename Value>
-inline typename LinearMap<Key, Value>::value_type * 
+inline typename LinearMap<Key, Value>::iterator
         LinearMap<Key, Value>:: findPtr(Key const &key) const
 {
-    value_type *ptr = const_cast<value_type *>(&*begin());
-
-    return std::find_if(ptr, ptr + size(), 
+    return std::find_if(begin(), end(),
         [&](value_type const &value)
         {
             return key == value.first; 
